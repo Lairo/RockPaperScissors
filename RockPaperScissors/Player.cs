d498bfc5f4d22941a1583b5e7dcf9ae1c355869a
chajus1 <chajus1@googlemail.com>
2023-01-27 19:17:41 +0000

    Creation Engine.cs, Player.cs. Player information about the player and (currently AI). Populated Program.cs with basic Idea and 1 choice of the game.

---
+++ RockPaperScissors/Player.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RockPaperScissors 
+{
+    internal class Player
+    {
+        public string Name { get; private set; }
+        public int Points { get; private set; }
+        public string Choice { get; set; }
+
+
+
+        public Player(string name, int points, string choice)
+        {
+            Name = name;
+            Points = points;
+            Choice = choice;
+         }
+        public string PlayerInfo
+        {
+            get { return $"Player {Name} has {Points} points."; }
+        }
+
+    }
+}
--- RockPaperScissors/Program.cs
+++ RockPaperScissors/Program.cs
@@ -3,13 +3,34 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+  //TODO: Menu 
+            // players
+            // game
+            // score
+            // save/load
+            // sets? 3 games in a row 10 games in a row
 namespace RockPaperScissors
 {
     internal class Program
     {
+        public static Player Bob = new Player("Bob", 0, "Hand");
+        public static Player AI = new Player("CPU", 10, "Hand");
         static void Main(string[] args)
         {
+            while (true)
+            {
+                Console.Write("\nSelect 'R' for Rock, 'P' for Paper,'S' for Scissors: ");
+                char key = Char.ToUpper(Console.ReadKey().KeyChar);
+                if (key != 'R' && key != 'S' && key != 'P' && key != '0') return;
+
+                switch (key)
+                {
+                    case 'R':
+
+                        Console.WriteLine($"\n\n{Bob.Name}<= {Bob.Choice} vs {AI.Choice} =>{AI.Name}'s " );
+                        break;
+                }
+            }
         }
     }
 }
